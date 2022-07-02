d0560b127b7d3f8c4bc708da49e3e5242f2eaef2
Lysa-Gomes <laysagomesmello9@gmail.com>
2022-07-02 19:26:44 -0300

    Funções

---
+++ script.js
@@ -0,0 +1,25 @@
+
+function sms(){
+    console.log('Olá! Esta mensagem está sendo exibida no console')
+}
+sms()
+
+function myName(names){
+    console.log(`Meu nome é ${names}`)
+}
+myName("Laysa Gomes");
+
+function sobre(nome, idade, estilo){
+    console.log(`Meu nome é ${nome} tenho ${idade} anos, e meu estilo musical preferido é ${estilo}`)
+}
+sobre("Laysa", 18, "pop e rock");
+
+function movie(filme, musica){
+    console.log(`Meu filme preferido é ${filme}, e minha música preferida é ${musica}`)
+}
+movie('Vingadores: A guerra infinita','Happier than ever');
+
+function valor(number){
+    console.log(`${number}`)
+}
+valor(10*3);
